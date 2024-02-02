5b33645cb0412ac031bdf3239dc5671734a7fdd7
Threemasks12 <jongeorgmagnussen@gmail.com>
2024-02-02 16:36:49 +0100

    Took away exstra written code that was not important.

--- task5.cs
+++ task5.cs
@@ -107,7 +107,7 @@ bool AtGoal()
 
 #endregion
 
-#region Created functions
+#region Created functions using known fuctions
 
 void center()
 {
@@ -130,57 +130,4 @@ void turnAround()
 }
 
 
-
-#region Basic functions
-// These functions are just her to make your intelisense work. 
-// They only have a conceptual function.
-
-void Move()
-{
-    // Moves the car 1 cell in the direction it is heading. 
-}
-
-void Turn()
-{
-    // Turns the car 90 deg counter clockwise.
-}
-
-bool Peek()
-{
-    // Returns true if the next cell is open, otherwise false.
-    return true; // Just a placeholder value. 
-}
-
-#endregion
-
-#region Created functions
-
-void SeeAlldirectins()
-{
-    int turnCount = 0;
-
-    while (turnCount < 4)
-    {
-        if (IsGoal())
-        {
-            Move();
-        }
-
-        Turn();
-
-        turnCount++;
-    }
-}
-
-bool IsGoal()
-{
-    // Returns true if the cell ahead is the goal cell.
-    return true; // Just a placeholder value.
-}
-
-bool AtGoal()
-{
-    // Returns true if the current cell is the goal cell.
-    return true; // just a placholder
-}
 
