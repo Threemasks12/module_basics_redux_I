c5edeb1c9d3747cfef0ba76b98a027bbfab6e2e0
Threemasks12 <jongeorgmagnussen@gmail.com>
2024-02-02 16:34:50 +0100

    Task 5 was easly done as the previous code worked for this task with small changes to the placements of turn after peeks

--- task5.cs
+++ task5.cs
@@ -10,22 +10,126 @@ Commit to github.
 
 ![alt](images/map5.png) 
 
-
-
 while (!AtGoal())
 {
-    if (Peek())
+    MoveOrTurn();
+}
+
+void MoveOrTurn();
+{
+    if(AtGoal())
     {
-        Move();
-        Mark();
+        return;
     }
-    else
+    
+    
+
+    Peek()
+    if(Peek())
+    {
+        Peek()
+        if(Peek())
+        {
+            Move()
+        }
+        else if(!Peek())
+        {
+            Turn();
+            Peek();
+            if(Peek())
+            {
+                Move();
+            }
+            else
+            {
+                Turn();
+            }
+        }
+    }
+    else if (!Peek())
     {
         Turn();
+        Peek();
+        if(Peek())
+        {
+            Move();
+        }
+        if(!Peek())
+        {
+            Turn();
+            Peek();
+            if(Peek())
+            {
+                Move();
+            }
+            else if(!Peek())
+            {
+                Turn();
+                Peek();
+                if(Peek())
+                {
+                    Move();
+                }
+            }
+        }
+        
     }
-
-    SeeAlldirectins();
+    
 }
+
+
+#region Basic functions
+// These functions are just her to make your intelisense work. 
+// They only have a conceptual function.
+
+void Move()
+{
+    // Moves the car 1 cell in the direction it is heading. 
+}
+
+void Turn()
+{
+    // Turns the car 90 deg clockwise.
+}
+
+bool Peek()
+{
+    // Returns true if the next cell is open, otherwise false.
+    return true; // Just a placeholder value. 
+}
+
+bool AtGoal()
+{
+    // Returns true if the current cell is the goal cell.
+    return true; // just a placholder
+}
+
+
+#endregion
+
+#region Created functions
+
+void center()
+{
+    Turn();
+    Turn();
+    Turn();
+}
+
+void trunLeft()
+{
+    Turn();
+    Turn();
+    Turn();
+}
+
+void turnAround()
+{
+    Turn();
+    Turn();
+}
+
+
 
 #region Basic functions
 // These functions are just her to make your intelisense work. 
